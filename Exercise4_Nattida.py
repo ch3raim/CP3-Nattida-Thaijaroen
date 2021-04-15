50516d329647fd545742c9641a96a402f0eb0c68
ch3raim <nutthida.tha@gmail.com>
2021-04-15 13:33:58 +0900

    Create EX4

---
+++ Exercise4_Nattida.py
@@ -0,0 +1,10 @@
+Founda_Eng = 80
+General_BN = 60.5
+Intro_CS = 72
+ComPro = 85.5
+
+print("--- Your Score ---")
+print("Foundation English                "," = ",Founda_Eng)
+print("Generall Business                 "," = ",General_BN)
+print("Introdution to Computer Systems   "," = ",Intro_CS)
+print("Computer Prohgramming             "," = ",ComPro)
