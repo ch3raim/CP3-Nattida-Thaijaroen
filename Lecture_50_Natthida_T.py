1e16756555a3fa550cccce82c621fba9217d3833
ch3raim <nutthida.tha@gmail.com>
2021-04-18 21:36:06 +0900

    Create Lecture_50_Natthida_T.py

---
+++ Lecture_50_Natthida_T.py
@@ -0,0 +1,16 @@
+def plusNumber(x, y):
+    print(x + y)
+
+def minusNumber(x, y):
+    print(x - y)
+
+def multioliedNumber(x, y):
+    print(x * y)
+
+def divideNumber(x, y):
+    print(x / y)
+
+plusNumber(2,10)
+minusNumber(2,10)
+multioliedNumber(2,10)
+divideNumber(2,10)
